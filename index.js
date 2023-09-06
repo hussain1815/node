fd19c7ac38c26a114cc83b583874fafe0bfca8d0
hussain sajid <hussainsajidpak@gmail.com>
2023-09-06 23:07:24 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var x = 10;
+var u = 30;
+
+if (x == 5) {
+    console.log("answer is matched")
+} else {
+    console.log("sorry")
+    console.log(x===x)
+}
+
+
+
+const arr = [1, 24, 5, 5, 415, 11, 1, 15, 5]
+console.log(arr[7])
